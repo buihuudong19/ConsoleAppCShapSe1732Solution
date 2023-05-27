e88271a92cee958dacc57d2f560973dbe523443c
dongbh <dongbh@gmail.com>
2023-05-27 11:10:36 +0700

    commit

--- ConsoleAppProductPRN211/Program.cs
+++ ConsoleAppProductPRN211/Program.cs
@@ -9,15 +9,20 @@ public class Program
     {
         IRepository repository = new Repository();
 
-        Product p1 = new(1,"Ram Gigabyte","Ram moi nhat nam 2023",123.55d);
-        Product p2 = new(2, "Ram Gigabyte", "Ram moi nhat nam 2023", 23);
-        Product p3 = new(3, "Ram Gigabyte", "Ram moi nhat nam 2023", 144.55d);
-        Product p4 = new(4, "Ram Gigabyte", "Ram moi nhat nam 2023", 2.55d);
+        Product p1 = new(1,"Ram Gigabyte","Ram moi nhat nam 2023",60);
+        Product p2 = new(2, "Ram Gigabyte", "Ram moi nhat nam 2023",60);
+        Product p3 = new(3, "Ram Gigabyte", "Ram moi nhat nam 2023", 50);
+        Product p4 = new(4, "Ram Gigabyte", "Ram moi nhat nam 2023", 40);
+
+        Product p5 = new(5, "Ram Gigabyte", "Ram moi nhat nam 2023", 40);
+        Product p6 = new(6, "Ram Gigabyte", "Ram moi nhat nam 2023", 30);
 
         repository.Add(p1);
         repository.Add(p2);
         repository.Add(p3);
         repository.Add(p4);
+        repository.Add(p5);
+        repository.Add(p6);
 
         /*
         Product[] products = repository.GetAll();
@@ -25,5 +30,11 @@ public class Program
         */
         repository.Display();
 
+    }
+
+
+    public static void ShowTop(Product[] products)
+    {
+        //hien thi top 3 san pham co price lon nhat
     }
 }
